959c6f799ee569bf3552dc2893e263ab91bb1697
Patrick Favre-Bulle <patrick.favrebulle@gmail.com>
2020-04-19 15:40:07 +0200

    Update to Java 11 and Spring Boot 2.2.6

--- src/test/java/at/favre/server/springsandbox/SpringSandboxApplicationTests.java
+++ src/test/java/at/favre/server/springsandbox/SpringSandboxApplicationTests.java
@@ -1,16 +1,14 @@
 package at.favre.server.springsandbox;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringSandboxApplicationTests {
+class DemoApplicationTests {
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
     }
 
 }
+
